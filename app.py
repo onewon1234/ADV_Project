b3bfcd6cb7aa635f1cbf942bc8a0e76a111ada74
onewon1234 <ihyewon771@gmail.com>
2025-07-18 17:15:06 +0900

    app.py port

--- app.py
+++ app.py
@@ -292,4 +292,6 @@ def result():
     return render_template("result.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True) 
+    app.run(host="0.0.0.0", port=8080)
+
+
